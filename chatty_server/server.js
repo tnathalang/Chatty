544050726074809801e99e172207c6445f62f940
vagrant <nathalang_t@hotmail.ca>
2019-02-15 01:22:37 +0000

    delete client ID when client closes tab

--- chatty_server/server.js
+++ chatty_server/server.js
@@ -114,5 +114,9 @@ wss.on('connection', (ws) => {
     })
 
     // Set up a callback for when a client closes the socket. This usually means they closed their browser.
-    ws.on('close', () => console.log('Client disconnected'));
+    ws.on('close', () => {
+        delete clientList[ws.id]
+        console.log('Client disconnected')
+    })
+
 });
